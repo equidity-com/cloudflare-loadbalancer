65aa0534bfb8a904bc8d398d09f72b129aa1232e
Prakash <prakash@equidity.com>
2026-07-22 10:13:37 +0400

    Update multi-app-config.js

--- multi-app-config.js
+++ multi-app-config.js
@@ -64,6 +64,7 @@ const PASSTHROUGH_DOMAINS = [
   'feeds.brokervu.com',
   'analytics.brokervu.com',
   'trade.brokervu.com',
+  '*.eqapi.brokervu.com',
   'api.brokervu.com',
   'chart-storage.brokervu.com'
 ];
